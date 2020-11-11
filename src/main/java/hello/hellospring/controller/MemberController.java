b04b37d449a69459f97e556e57aaca1986db1364
pentas1150 <beewol@soongsil.ac.kr>
2020-11-11 21:28:23 +0900

    의존관계 자동 및 수동 추가

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -8,10 +8,17 @@ import org.springframework.stereotype.Controller;
 public class MemberController {
     private final MemberService memberService;
 
+    //생성자 주입(DI) -> 이게 제일 Best
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    //세터 주입(DI)
+    //    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
 
 
 }
